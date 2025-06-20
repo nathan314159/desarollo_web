d7c5e4d8289c72a4fc2a77320dcd3641f96633e8
nathan314159 <nathan_arboleda@hotmail.com>
2025-06-20 10:33:17 -0500

    feature/creacion de la pagina

---
+++ semana_6 Trabajo consulta CSS/script.js
@@ -0,0 +1,9 @@
+        // Función que muestra un div (caja informativa) según su ID
+        function mostrar(id) {
+            document.getElementById(id).style.display = 'block';
+        }
+
+        // Función que oculta un div (caja informativa) según su ID
+        function cerrar(id) {
+            document.getElementById(id).style.display = 'none';
+        }
