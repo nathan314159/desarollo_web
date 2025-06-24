83de7f3b6835f6f2c0ab783a878953e1bdae4363
nathan314159 <nathan_arboleda@hotmail.com>
2025-06-24 15:41:11 -0500

    deleted

---
+++ proyectoDW/app/Controllers/DeberSemana8.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Deber_semena_8;
+use App\Controllers\BaseController;
+class DeberSemena8 extends BaseController
+{
+    public function tablaDeMultiplicar()
+    {
+        return view("/DeberSemana8/tablaMultiplicar");
+    }
+
+
+}
